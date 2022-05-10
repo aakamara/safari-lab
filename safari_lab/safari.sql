1982011eff4db11d71882b3db450584134f5fc71
Amber Andrews-Kamara <aandrews.k@gmail.com>
2022-05-10 14:53:40 +0100

    creates table for staff

--- safari_lab/safari.sql
+++ safari_lab/safari.sql
@@ -2,3 +2,9 @@ DROP TABLE assignments;
 DROP TABLE animals;
 DROP TABLE enclosures;
 DROP TABLE staff;
+
+CREATE TABLE staff(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(255),
+    staff_number INT
+);
